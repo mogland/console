bed5cf2d4cca2b50dac75db0faa79dd1e55f714b
wibus-wee <1596355173@qq.com>
2022-07-14 16:27:30 +0800

    chore(app): reset project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { Scrollbar } from './components/universal/Scrollbar'
-import Sidebar from './components/widgets/Sidebar'
+
 import { AppRouter } from './router'
-import { NxLayout } from './components/universal/Layout'
 
 function App() {
 
   return (
     <>
-      <div><Sidebar /></div>
-      <NxLayout style={{left: 250}}>
-        <Scrollbar>
-          <AppRouter />
-        </Scrollbar>
-      </NxLayout>
+      <AppRouter />
     </>
   )
 }
--- src/router.tsx
+++ src/router.tsx
@@ -3,20 +3,18 @@
  * @author: Wibus
  * @Date: 2022-07-12 16:25:35
  * @LastEditors: Wibus
- * @LastEditTime: 2022-07-12 16:29:20
+ * @LastEditTime: 2022-07-14 16:27:16
  * Coding With IU
  */
 
 import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { Posts } from "./pages/Posts"
 
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/posts" element={<Posts />} />
+      {/* <Route path="/" element={<Home />} />
+      <Route path="/posts" element={<Posts />} /> */}
     </Routes>
   )
 }
