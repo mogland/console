3a701e0f133e6e3f2cda7c20ff4721e74ea15250
wibus-wee <1596355173@qq.com>
2022-07-14 18:08:46 +0800

    chore(dashboard): modify title

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,18 +3,17 @@
  * @author: Wibus
  * @Date: 2022-07-14 16:30:25
  * @LastEditors: Wibus
- * @LastEditTime: 2022-07-14 16:41:47
+ * @LastEditTime: 2022-07-14 18:07:51
  * Coding With IU
  */
 
-import { Page, Button, Text } from "@geist-ui/core"
-
+import { Page, Button, Text, Grid, Card } from "@geist-ui/core"
+import styles from "./index.module.css"
 
 export const Dashboard = () => {
   return (
-    <Page>
-      <Text h1>Home Page</Text>
-      <Button>Submit</Button>
+    <Page className={styles.page}>
+      <Text h1>Dashboard</Text>
     </Page>
   )
 }
