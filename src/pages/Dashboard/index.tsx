48c794418ec3efc7ed85f730be37f99b0fae3fc7
wibus-wee <1596355173@qq.com>
2022-07-14 18:38:44 +0800

    feat(dashboard): basic view container

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,7 +3,7 @@
  * @author: Wibus
  * @Date: 2022-07-14 16:30:25
  * @LastEditors: Wibus
- * @LastEditTime: 2022-07-14 18:07:51
+ * @LastEditTime: 2022-07-14 18:36:16
  * Coding With IU
  */
 
@@ -13,7 +13,28 @@ import styles from "./index.module.css"
 export const Dashboard = () => {
   return (
     <Page className={styles.page}>
-      <Text h1>Dashboard</Text>
+      <div className={styles.header}>
+        <Text h2>Dashboard</Text>
+      </div>
+      <div className={styles.viewContainer}>
+        <section className={styles.dashboardArea + " " + styles.mixed}>
+          <div className={styles.dashboardContainer}>
+            <div className={styles.dashboardBox + " " + styles.blogPost}>
+              <h2>开始书写你的文章</h2>
+
+              <a href="#/members/" >
+                {/* <span className={styles.icon}></span> */}
+                <div>
+                  <h4>发布第一篇文章</h4>
+                  <p>让你对 NEXT 的编辑器有一个初步了解</p>
+                </div>
+              </a>
+
+            </div>
+          </div>
+        </section>
+
+      </div>
     </Page>
   )
 }
