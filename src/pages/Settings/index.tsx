bd2254fa699a44a954918bc8f7c7a63527cbe601
wibus-wee <1596355173@qq.com>
2022-08-03 00:13:22 +0800

    feat(settings): fetching setting data to core

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -3,7 +3,7 @@
  * @author: Wibus
  * @Date: 2022-08-02 20:51:21
  * @LastEditors: Wibus
- * @LastEditTime: 2022-08-02 21:38:53
+ * @LastEditTime: 2022-08-03 00:12:30
  * Coding With IU
  */
 
@@ -14,6 +14,9 @@ import Dashboards from "../../components/layouts/Dashboards";
 import { Save } from "@geist-ui/icons";
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { apiClient } from "../../utils/request";
+import { useMount } from "react-use";
+import { message } from "react-message-popup";
 
 export const Settings: BasicPage = () => {
   const appNavigate = useNavigate()
@@ -22,12 +25,39 @@ export const Settings: BasicPage = () => {
   const params = new URLSearchParams(search)
   const [nowTab, setNowTab] = useState(params.get("tab") || "1")
 
+  const [user, setUser] = useState<any>({})
+  const [configs, setConfigs] = useState<any>({})
+
+  const request = async () => {
+    await apiClient.get('/user').then(res => {
+      setUser(res)
+    })
+    await apiClient.get('/configs').then(res => {
+      console.log(res)
+      setConfigs(res)
+    })
+  }
+
+  useMount(() => {
+    request()
+  })
+
   return (
     <NxPage title={"Settings"}>
       <Dashboards.Container className="lg:grid flex flex-col" gridTemplateColumns='1fr'>
         <Dashboards.Area className={useClasses("overflow-x-hidden")} style={{ overflow: "auto" }}>
           <Button shadow type="success"
-
+            onClick={async () => {
+              await apiClient.patch('/user', null, null, JSON.stringify(user)).then(res => {
+                message.success("用户配置保存成功")
+                request()
+              })
+              
+              await apiClient.patch('/configs', null, null, JSON.stringify(configs)).then(res => {
+                message.success("系统配置保存成功")
+                request()
+              })
+            }}
             style={{
               borderRadius: "100%",
               width: "50px",
@@ -80,7 +110,7 @@ export const Settings: BasicPage = () => {
                         position: 'relative',
                         userSelect: 'none',
                       }}>
-                        <img alt="" src="https://cdn.my-api.cn/user/w/asset-pic-gitee/20220312201745.png" style={{
+                        <img alt={user.username} src={user.avatar} style={{
                           height: "100%",
                           maxWidth: "100%",
                           borderRadius: "100%",
@@ -88,25 +118,56 @@ export const Settings: BasicPage = () => {
                         }} />
                       </div>
                       <Spacer />
-                      
-                      wibus
+                      最近登录IP: {user.last_login_ip}
+                      <Spacer />
+                      最近登录时间: {user.last_login_time?.split("T")[0]}
                     </div>
                   </div>
                 </div>
                 <div style={{
                   marginTop: "100px",
                 }}>
-                  <Input label="用户名" placeholder="请输入用户名" width={"100%"} />
+                  <Input label="用户名" placeholder="请输入用户名" width={"100%"} initialValue={user.username} onChange={(e) => {
+                    setUser({
+                      ...user,
+                      username: e.target.value,
+                    })
+                  }} />
                   <Spacer />
-                  <Input label="昵称" placeholder="请输入昵称" width={"100%"} />
+                  <Input label="昵称" placeholder="请输入昵称" width={"100%"} initialValue={user.name} onChange={(e) => {
+                    setUser({
+                      ...user,
+                      name: e.target.value,
+                    })
+                  }} />
                   <Spacer />
-                  <Input label="邮箱" placeholder="请输入邮箱" width={"100%"} />
+                  <Input label="邮箱" placeholder="请输入邮箱" width={"100%"} initialValue={user.mail} onChange={(e) => {
+                    setUser({
+                      ...user,
+                      mail: e.target.value,
+                    })
+                  }} />
                   <Spacer />
-                  <Input label="个人首页" placeholder="请输入个人首页" width={"100%"} />
+                  <Input label="个人首页" placeholder="请输入个人首页" width={"100%"} initialValue={user.url} onChange={(e) => {
+                    setUser({
+                      ...user,
+                      url: e.target.value,
+                    })
+                  }} />
                   <Spacer />
-                  <Input label="头像" placeholder="请输入头像" width={"100%"} />
+                  <Input label="头像" placeholder="请输入头像" width={"100%"} initialValue={user.avatar} onChange={(e) => {
+                    setUser({
+                      ...user,
+                      avatar: e.target.value,
+                    })
+                  }} />
                   <Spacer />
-                  <Textarea placeholder="请输入个人介绍"width={"100%"} ></Textarea>
+                  <Textarea placeholder="请输入个人介绍"width={"100%"} initialValue={user.introduce} onChange={(e) => {
+                    setUser({
+                      ...user,
+                      introduce: e.target.value,
+                    })
+                  }} ></Textarea>
                 </div>
               </div>
 
@@ -118,10 +179,34 @@ export const Settings: BasicPage = () => {
                     gridTemplateColumns: "1fr 1fr",
                     gridGap: "1.8vw"
                   }}>
-                    <Input clearable label="前端地址" width={"100%"} />
-                    <Input clearable label="API 地址" width={"100%"} />
-                    <Input clearable label="后台地址" width={"100%"} />
-                    <Input clearable label="Gateway 地址" width={"100%"} />
+                    <Input clearable label="前端地址" width={"100%"} initialValue={configs.urls?.web_url} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        urls: {
+                          ...configs.urls,
+                          web_url: e.target.value,
+                        }
+                      })
+                    }} />
+                    <Input clearable label="API 地址" width={"100%"} initialValue={configs.urls?.core_url} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        urls: {
+                          ...configs.urls,
+                          core_url: e.target.value,
+                        }
+                      })
+                    }} />
+                    <Input clearable label="后台地址" width={"100%"} initialValue={configs.urls?.admin_url} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        urls: {
+                          ...configs.urls,
+                          admin_url: e.target.value,
+                        }
+                      })
+                    }} />
+                    {/* <Input clearable label="Gateway 地址" width={"100%"} /> */}
                   </div>
                 </Collapse>
                 <Collapse title="站点设置">
@@ -129,10 +214,35 @@ export const Settings: BasicPage = () => {
                     gridTemplateColumns: "1fr 1fr",
                     gridGap: "1.8vw"
                   }}>
-                    <Input clearable label="站点名称" width={"100%"} />
-                    <Input clearable label="站点描述" width={"100%"} />
-                    <Input clearable label="站点图标" width={"100%"} />
-                    <Input clearable label="站点关键字" width={"100%"} />
+                    <Input clearable label="站点名称" width={"100%"} initialValue={configs.site?.title} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        site: {
+                          ...configs.site,
+                          title: e.target.value,
+                        }
+                      })
+                    }} />
+                    <Input clearable label="站点描述" width={"100%"} initialValue={configs.site?.description} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        site: {
+                          ...configs.site,
+                          description: e.target.value,
+                        }
+                      })
+                    }} />
+                    {/* <Input clearable label="站点图标" width={"100%"} /> */}
+                    {/* configs.site.keywords.split(",") */}
+                    <Input clearable label="站点关键字" width={"100%"} initialValue={configs.site?.keywords?.join(",")} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        site: {
+                          ...configs.site,
+                          keywords: e.target.value?.split(","),
+                        }
+                      })
+                    }} />
                   </div>
                 </Collapse>
                 <Collapse title="邮件设置">
@@ -143,10 +253,48 @@ export const Settings: BasicPage = () => {
                     gridGap: "1.8vw"
                   }}>
 
-                    <Input clearable label="邮件服务器" width={"100%"} />
-                    <Input clearable label="邮件端口" width={"100%"} />
-                    <Input clearable label="邮件用户名" width={"100%"} />
-                    <Input clearable label="邮件密码" width={"100%"} />
+                    <Input clearable label="邮件服务器" width={"100%"} initialValue={configs.mail_options?.host} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        mail_options: {
+                          ...configs.mail_options,
+                          host: e.target.value,
+                        }
+                      })
+                    }} />
+                    <Input clearable label="邮件端口" width={"100%"} initialValue={configs.mail_options?.port} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        mail_options: {
+                          ...configs.mail_options,
+                          port: e.target.value,
+                        }
+                      })
+                    }} />
+                    <Input clearable label="邮件用户名" width={"100%"} initialValue={configs.mail_options?.auth?.user} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        mail_options: {
+                          ...configs.mail_options,
+                          auth: {
+                            ...configs.mail_options.auth,
+                            user: e.target.value,
+                          }
+                        }
+                      })
+                    }} />
+                    <Input.Password clearable label="邮件密码" width={"100%"} initialValue={configs.mail_options?.auth?.pass} onChange={(e) => {
+                      setConfigs({
+                        ...configs,
+                        mail_options: {
+                          ...configs.mail_options,
+                          auth: {
+                            ...configs.mail_options.auth,
+                            pass: e.target.value,
+                          }
+                        }
+                      })
+                    }} />
                   </div>
                 </Collapse>
                 {/* <Collapse title="后台设置">
@@ -155,17 +303,29 @@ export const Settings: BasicPage = () => {
               </Collapse.Group>
             </Tabs.Item>
             <Tabs.Item label="安全" value="3">
-              <Collapse.Group>
+              <Collapse.Group width={"100%"}>
                 <Collapse title="修改密码">
                   <div className="grid" style={{
-                    // gridTemplateColumns: "1fr 1fr",
+                    gridTemplateColumns: "1fr 1fr",
                     gridGap: "1.8vw"
                   }}>
-                    <Input.Password clearable label="新密码" width={"100%"} />
-                    <Input.Password clearable label="确认密码" width={"100%"} />
+                    <Input.Password clearable label="新密码" id="new-password" />
+                    <Input.Password clearable label="确认密码"id="new-password-confirm" />
                   </div>
                   <Spacer />
-                  <Button type="success" style={{
+                  <Button type="success" 
+                  onClick={async () => {
+                    if (document.getElementById("new-password")!.value === document.getElementById("new-password-confirm")!.value) {
+                      await apiClient.patch("/user", null, null, {
+                        password: document.getElementById("new-password")!.value,
+                      }).then(() => {
+                        message.success("密码修改成功")
+                      })
+                    } else {
+                      message.error("两次密码不一致")
+                    }
+                  }}
+                  style={{
                     float: 'right',
                     // width: '50px',
                     minWidth: '70px',
