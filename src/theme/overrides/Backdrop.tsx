c5b77ddfc8ae0f9ba07b7cc73f22ed04960b9a09
wibus-wee <1596355173@qq.com>
2022-10-05 15:34:08 +0800

    feat(theme): `Backdrop` override component

---
+++ src/theme/overrides/Backdrop.tsx
@@ -0,0 +1,34 @@
+/*
+ * @FilePath: /mog-admin/src/theme/overrides/Backdrop.tsx
+ * @author: Wibus
+ * @Date: 2022-10-05 15:31:17
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-10-05 15:31:17
+ * Coding With IU
+ */
+import { alpha } from '@mui/material/styles';
+
+// ----------------------------------------------------------------------
+
+export default function Backdrop(theme) {
+  const varLow = alpha(theme.palette.grey[900], 0.48);
+  const varHigh = alpha(theme.palette.grey[900], 1);
+
+  return {
+    MuiBackdrop: {
+      styleOverrides: {
+        root: {
+          background: [
+            `rgb(22,28,36)`,
+            `-moz-linear-gradient(75deg, ${varLow} 0%, ${varHigh} 100%)`,
+            `-webkit-linear-gradient(75deg, ${varLow} 0%, ${varHigh} 100%)`,
+            `linear-gradient(75deg, ${varLow} 0%, ${varHigh} 100%)`,
+          ],
+          '&.MuiBackdrop-invisible': {
+            background: 'transparent',
+          },
+        },
+      },
+    },
+  };
+}
