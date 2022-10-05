dd8b00e7682ff6dc1ac212eefeeda87c9689bb80
wibus-wee <1596355173@qq.com>
2022-10-05 15:56:03 +0800

    feat: collapse drawer context

---
+++ src/contexts/CollapseDrawerContext.tsx
@@ -0,0 +1,80 @@
+/*
+ * @FilePath: /mog-admin/src/contexts/CollapseDrawerContext.tsx
+ * @author: Wibus
+ * @Date: 2022-10-05 15:52:42
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-10-05 15:53:50
+ * Coding With IU
+ */
+import PropTypes from 'prop-types';
+import { createContext, useState, useEffect } from 'react';
+// hooks
+import useResponsive from '../hooks/useResponsive';
+
+// ----------------------------------------------------------------------
+
+const initialState = {
+  collapseClick: false,
+  collapseHover: false,
+  onToggleCollapse: () => {},
+  onHoverEnter: () => {},
+  onHoverLeave: () => {},
+  isCollapse: false,
+};
+
+const CollapseDrawerContext = createContext(initialState);
+
+// ----------------------------------------------------------------------
+
+CollapseDrawerProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+function CollapseDrawerProvider({ children }) {
+  const isDesktop = useResponsive('up', 'lg');
+
+  const [collapse, setCollapse] = useState({
+    click: false,
+    hover: false,
+  });
+
+  useEffect(() => {
+    if (!isDesktop) {
+      setCollapse({
+        click: false,
+        hover: false,
+      });
+    }
+  }, [isDesktop]);
+
+  const handleToggleCollapse = () => {
+    setCollapse({ ...collapse, click: !collapse.click });
+  };
+
+  const handleHoverEnter = () => {
+    if (collapse.click) {
+      setCollapse({ ...collapse, hover: true });
+    }
+  };
+
+  const handleHoverLeave = () => {
+    setCollapse({ ...collapse, hover: false });
+  };
+
+  return (
+    <CollapseDrawerContext.Provider
+      value={{
+        isCollapse: collapse.click && !collapse.hover,
+        collapseClick: collapse.click,
+        collapseHover: collapse.hover,
+        onToggleCollapse: handleToggleCollapse,
+        onHoverEnter: handleHoverEnter,
+        onHoverLeave: handleHoverLeave,
+      }}
+    >
+      {children}
+    </CollapseDrawerContext.Provider>
+  );
+}
+
+export { CollapseDrawerProvider, CollapseDrawerContext };
