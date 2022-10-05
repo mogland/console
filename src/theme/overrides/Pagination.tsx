34ba1affccaf6d2ccb34ceb54ae2fdb09072ecd8
wibus-wee <1596355173@qq.com>
2022-10-05 15:43:42 +0800

    feat(theme): `Pagination` override component

---
+++ src/theme/overrides/Pagination.tsx
@@ -0,0 +1,43 @@
+/*
+ * @FilePath: /mog-admin/src/theme/overrides/Pagination.tsx
+ * @author: Wibus
+ * @Date: 2022-10-05 15:41:16
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-10-05 15:41:16
+ * Coding With IU
+ */
+import { alpha } from '@mui/material/styles';
+
+// ----------------------------------------------------------------------
+
+export default function Pagination(theme) {
+  return {
+    MuiPaginationItem: {
+      styleOverrides: {
+        root: {
+          '&.Mui-selected': {
+            fontWeight: theme.typography.fontWeightBold,
+          },
+        },
+        textPrimary: {
+          '&.Mui-selected': {
+            color: theme.palette.primary.main,
+            backgroundColor: alpha(theme.palette.primary.main, 0.08),
+            '&:hover, &.Mui-focusVisible': {
+              backgroundColor: `${alpha(theme.palette.primary.main, 0.24)} !important`,
+            },
+          },
+        },
+        outlined: {
+          border: `1px solid ${theme.palette.grey[500_32]}`,
+        },
+        outlinedPrimary: {
+          '&.Mui-selected': {
+            backgroundColor: alpha(theme.palette.primary.main, 0.08),
+            border: `1px solid ${alpha(theme.palette.primary.main, 0.24)}`,
+          },
+        },
+      },
+    },
+  };
+}
