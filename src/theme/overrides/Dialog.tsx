dc2911cc2557791ee241800ccdd1a81f549ae47a
wibus-wee <1596355173@qq.com>
2022-10-05 15:38:53 +0800

    feat(theme): `Dialog` override component

---
+++ src/theme/overrides/Dialog.tsx
@@ -0,0 +1,66 @@
+/*
+ * @FilePath: /mog-admin/src/theme/overrides/Dialog.tsx
+ * @author: Wibus
+ * @Date: 2022-10-05 15:36:41
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-10-05 15:36:41
+ * Coding With IU
+ */
+
+
+export default function Dialog(theme) {
+  return {
+    MuiDialog: {
+      styleOverrides: {
+        paper: {
+          boxShadow: theme.customShadows.dialog,
+          '&.MuiPaper-rounded': {
+            borderRadius: Number(theme.shape.borderRadius) * 2,
+          },
+          '&.MuiDialog-paperFullScreen': {
+            borderRadius: 0,
+          },
+          '&.MuiDialog-paper .MuiDialogActions-root': {
+            padding: theme.spacing(3),
+          },
+          '@media (max-width: 600px)': {
+            margin: theme.spacing(2),
+          },
+          '@media (max-width: 663.95px)': {
+            '&.MuiDialog-paperWidthSm.MuiDialog-paperScrollBody': {
+              maxWidth: '100%',
+            },
+          },
+        },
+        paperFullWidth: {
+          width: '100%',
+        },
+      },
+    },
+    MuiDialogTitle: {
+      styleOverrides: {
+        root: {
+          padding: theme.spacing(3, 3, 0),
+        },
+      },
+    },
+    MuiDialogContent: {
+      styleOverrides: {
+        root: {
+          borderTop: 0,
+          borderBottom: 0,
+          padding: theme.spacing(3),
+        },
+      },
+    },
+    MuiDialogActions: {
+      styleOverrides: {
+        root: {
+          '& > :not(:first-of-type)': {
+            marginLeft: theme.spacing(1.5),
+          },
+        },
+      },
+    },
+  };
+}
