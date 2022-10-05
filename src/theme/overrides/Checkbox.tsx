027bc89684bb183024b6e15d6e4d4d84c50b9b43
wibus-wee <1596355173@qq.com>
2022-10-05 15:34:39 +0800

    feat(theme): `Checkbox` override component

---
+++ src/theme/overrides/Checkbox.tsx
@@ -0,0 +1,47 @@
+/*
+ * @FilePath: /mog-admin/src/theme/overrides/Checkbox.tsx
+ * @author: Wibus
+ * @Date: 2022-10-05 15:32:06
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-10-05 15:32:07
+ * Coding With IU
+ */
+//
+import { CheckboxIcon, CheckboxCheckedIcon, CheckboxIndeterminateIcon } from './CustomIcons';
+
+// ----------------------------------------------------------------------
+
+export default function Checkbox(theme) {
+  return {
+    MuiCheckbox: {
+      defaultProps: {
+        icon: <CheckboxIcon />,
+        checkedIcon: <CheckboxCheckedIcon />,
+        indeterminateIcon: <CheckboxIndeterminateIcon />,
+      },
+
+      styleOverrides: {
+        root: {
+          padding: theme.spacing(1),
+          '&.Mui-checked.Mui-disabled, &.Mui-disabled': {
+            color: theme.palette.action.disabled,
+          },
+          '& .MuiSvgIcon-fontSizeMedium': {
+            width: 24,
+            height: 24,
+          },
+          '& .MuiSvgIcon-fontSizeSmall': {
+            width: 20,
+            height: 20,
+          },
+          svg: {
+            fontSize: 24,
+            '&[font-size=small]': {
+              fontSize: 20,
+            },
+          },
+        },
+      },
+    },
+  };
+}
