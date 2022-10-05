f309e4a38a6ca7bf6f9c27ca071ae8ba3a850977
wibus-wee <1596355173@qq.com>
2022-10-05 15:35:03 +0800

    chore: assign all overrides components

--- src/theme/overrides/index.tsx
+++ src/theme/overrides/index.tsx
@@ -3,17 +3,35 @@
  * @author: Wibus
  * @Date: 2022-10-05 15:10:06
  * @LastEditors: Wibus
- * @LastEditTime: 2022-10-05 15:27:28
+ * @LastEditTime: 2022-10-05 15:33:29
  * Coding With IU
  */
 
 import Accordion from "./Accordion";
 import Alert from "./Alert";
+import Autocomplete from "./Autocomplete";
+import Avatar from "./Avatar";
+import Backdrop from "./Backdrop";
+import Breadcrumbs from "./Breadcrumbs";
+import Button from "./Button";
+import ButtonGroup from "./ButtonGroup";
+import Card from "./Card";
+import Checkbox from "./Checkbox";
+import Chip from "./Chip";
 
 
 export default function ComponentsOverrides(theme) {
   return Object.assign(
     Accordion(theme),
     Alert(theme),
+    Autocomplete(theme),
+    Avatar(theme),
+    Backdrop(theme),
+    Breadcrumbs(theme),
+    Button(theme),
+    ButtonGroup(theme),
+    Card(theme),
+    Checkbox(theme),
+    Chip(theme),
   )
 }
