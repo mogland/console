182a5327d5ad439276ea2fd3007d710b029b91ed
wibus-wee <1596355173@qq.com>
2022-10-05 08:34:34 +0800

    revert: restart console project

--- src/utils/env.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @FilePath: /nx-admin/src/utils/env.ts
- * @author: Wibus
- * @Date: 2022-07-15 20:54:24
- * @LastEditors: Wibus
- * @LastEditTime: 2022-07-15 20:54:25
- * Coding With IU
- */
-export const isDev = process.env.NODE_ENV === "development";
