621f4f9378b410f1b9d543935c17e4801ae7e3db
wibus-wee <1596355173@qq.com>
2022-10-06 13:25:13 +0800

    feat(hooks): `useLocales` hook

---
+++ src/hooks/useLocales.ts
@@ -0,0 +1,31 @@
+import { useTranslation } from 'react-i18next';
+// '@mui
+import { enUS, deDE, frFR } from '@mui/material/locale';
+
+// ----------------------------------------------------------------------
+
+const LANGS = [
+  {
+    label: 'English',
+    value: 'en',
+    systemValue: enUS,
+    icon: 'https://minimal-assets-api.vercel.app/assets/icons/ic_flag_en.svg',
+  },
+];
+
+export default function useLocales() {
+  const { i18n, t: translate } = useTranslation();
+  const langStorage = localStorage.getItem('i18nextLng');
+  const currentLang = LANGS.find((_lang) => _lang.value === langStorage) || LANGS[1];
+
+  const handleChangeLanguage = (newlang) => {
+    i18n.changeLanguage(newlang);
+  };
+
+  return {
+    onChangeLang: handleChangeLanguage,
+    translate,
+    currentLang,
+    allLang: LANGS,
+  };
+}
