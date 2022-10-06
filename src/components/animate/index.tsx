d31baf8ec6d16e673bf4fb019ce93ca50e2372f9
wibus-wee <1596355173@qq.com>
2022-10-06 18:00:34 +0800

    feat(components ): `IconButtonAnimate` animate component

---
+++ src/components/animate/index.tsx
@@ -0,0 +1,10 @@
+/*
+ * @FilePath: /mog-admin/src/components/animate/index.tsx
+ * @author: Wibus
+ * @Date: 2022-10-06 17:57:42
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-10-06 17:57:42
+ * Coding With IU
+ */
+
+export { default as IconButtonAnimate } from './IconButtonAnimate';
