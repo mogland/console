cb610c37349d838060c69930a4930b146ca6c9a8
Wibus <62133302+wibus-wee@users.noreply.github.com>
2022-10-06 12:11:26 +0800

    feat(layout): `main`and `Logo` layout (#58)
    * feat(layout): user log out with only logo
    
    * feat(layout): the `menu` for desktop and mobile
    
    * feat(layout): `Header` and `Footer` for main layout
    
    * feat(layout): basic main layout
    
    * fix: file name typo

---
+++ src/layouts/LogoOnlyLayout.tsx
@@ -0,0 +1,40 @@
+/*
+ * @FilePath: /mog-admin/src/layouts/LogoOnlyLayout.tsx
+ * @author: Wibus
+ * @Date: 2022-10-06 09:35:47
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-10-06 10:14:38
+ * Coding With IU
+ */
+import { Outlet } from 'react-router-dom';
+// @mui
+import { styled } from '@mui/material/styles';
+// components
+// import Logo from '../components/Logo';
+
+// ----------------------------------------------------------------------
+
+const HeaderStyle = styled('header')(({ theme }) => ({
+  top: 0,
+  left: 0,
+  lineHeight: 0,
+  width: '100%',
+  position: 'absolute',
+  padding: theme.spacing(3, 3, 0),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(5, 5, 0)
+  }
+}));
+
+// ----------------------------------------------------------------------
+
+export default function LogoOnlyLayout() {
+  return (
+    <>
+      <HeaderStyle>
+        {/* <Logo /> */}
+      </HeaderStyle>
+      <Outlet />
+    </>
+  );
+}
