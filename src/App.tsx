9d492732ed37c2fa85c1f84121d880d9a8819b76
wibus-wee <1596355173@qq.com>
2022-10-22 08:43:17 +0800

    perf: use theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { useState } from 'react'
-
-import './App.css'
+import Router from "./routes"
+import ThemeProvider from "./theme"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeProvider>
+      <Router />
+    </ThemeProvider>
   )
 }
 
