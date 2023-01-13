6bef3d9ec50ef3b04657d44c18e95fd4247a8d9a
wibus-wee <1596355173@qq.com>
2023-01-13 19:20:37 +0800

    feat(modal): support `onCancel` and preitter lint

--- src/components/universal/Modal/index.tsx
+++ src/components/universal/Modal/index.tsx
@@ -1,60 +1,71 @@
-import styles from "./index.module.css"
-import { Fragment, useState } from "react"
-import { Dialog, Transition } from "@headlessui/react"
-import clsx from "clsx"
-export const Modal = ({ children, onClose, title, type, options, onConfirm, size, doubleClick }: {
-  children: React.ReactNode,
-  title: string,
-  type?: "confirm" | "info",
+import styles from "./index.module.css";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import clsx from "clsx";
+export const Modal = ({
+  children,
+  onClose,
+  title,
+  type,
+  options,
+  onConfirm,
+  size,
+  doubleClick,
+  onCancel,
+}: {
+  children: React.ReactNode;
+  title: string;
+  type?: "confirm" | "info";
   options?: {
-    confirmText?: string,
-    cancelText?: string,
-  }
-  onConfirm?: (e: boolean) => void,
-  onClose?: (e: boolean) => void,
-  size?: "sm" | "md" | "lg",
+    confirmText?: string;
+    cancelText?: string;
+  };
+  onConfirm?: (e: boolean) => void;
+  onCancel?: (e: boolean) => void;
+  onClose?: (e: boolean) => void;
+  size?: "sm" | "md" | "lg";
   doubleClick?: {
-    confirm?: boolean,
-    cancel?: boolean,
-  },
+    confirm?: boolean;
+    cancel?: boolean;
+  };
 }) => {
-  const [isOpen, setIsOpen] = useState(true)
-  const [doubleClickState, setDoubleClickState] = useState(doubleClick || {
-    confirm: false,
-    cancel: false,
-  })
+  const [isOpen, setIsOpen] = useState(true);
+  const [doubleClickState, setDoubleClickState] = useState({
+    confirm: doubleClick?.confirm || false,
+    cancel: doubleClick?.cancel || false,
+  });
 
   const confirm = (e) => {
-    console.log(doubleClickState)
     if (doubleClickState.confirm) {
-      e.preventDefault()
-      e.currentTarget.classList.add(styles["double-click"])
+      e.preventDefault();
+      e.currentTarget.classList.add(styles["double-click"]);
       setDoubleClickState({
         ...doubleClickState,
         confirm: false,
-      })
+      });
       return;
     }
-    onConfirm?.(true)
-  }
+    onClose?.(true);
+    onConfirm?.(true);
+  };
 
   const cancel = (e) => {
     if (doubleClickState.cancel) {
-      e.preventDefault()
-      e.currentTarget.classList.add(styles["double-click"])
+      e.preventDefault();
+      e.currentTarget.classList.add(styles["double-click"]);
       setDoubleClickState({
         ...doubleClickState,
         cancel: false,
-      })
-      return
+      });
+      return;
     }
-
-    onClose?.(false)
-  }
+    onClose?.(false);
+    onCancel?.(false);
+  };
 
   function closeModal(e: boolean) {
-    setIsOpen(false)
-    onClose?.(e)
+    setIsOpen(false);
+    onClose?.(e);
   }
   return (
     <Transition appear show={isOpen} as={Fragment}>
@@ -83,36 +94,30 @@ export const Modal = ({ children, onClose, title, type, options, onConfirm, size
               leaveTo="opacity-0 scale-95"
             >
               <Dialog.Panel
-                className={clsx(styles.panel, styles[size || "sm"])}>
-                <Dialog.Title
-                  as="h3"
-                  className={clsx(styles.title)}
-                >
+                className={clsx(styles.panel, styles[size || "sm"])}
+              >
+                <Dialog.Title as="h3" className={clsx(styles.title)}>
                   {title}
                 </Dialog.Title>
-                <div className="mt-2">
-                  {children}
-                </div>
+                <div className="mt-2">{children}</div>
                 <div className="mt-4">
-                  {
-                    type === "confirm" && (
-                      <button
-                        type="button"
-                        className={clsx(styles.confirm, styles.button)}
-                        onClick={(e) => {
-                          confirm(e)
-                        }}
-                      >
-                        {options?.confirmText || "好的～"}
-                      </button>
-                    )
-                  }
+                  {type === "confirm" && (
+                    <button
+                      type="button"
+                      className={clsx(styles.confirm, styles.button)}
+                      onClick={(e) => {
+                        confirm(e);
+                      }}
+                    >
+                      {options?.confirmText || "好的～"}
+                    </button>
+                  )}
 
                   <button
                     type="button"
                     className={clsx(styles.cancel, styles.button)}
                     onClick={(e) => {
-                      cancel(e)
+                      cancel(e);
                     }}
                   >
                     {options?.cancelText || "取消"}
@@ -124,13 +129,9 @@ export const Modal = ({ children, onClose, title, type, options, onConfirm, size
         </div>
       </Dialog>
     </Transition>
-  )
-}
+  );
+};
 
 export const ModalBody = ({ children }) => {
-  return (
-    <p className={clsx(styles.modalBody)}>
-      {children}
-    </p>
-  )
-}
+  return <p className={clsx(styles.modalBody)}>{children}</p>;
+};
