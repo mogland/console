c5ecf94322634a8a3f747c692b8d1207f13db89c
wibus-wee <1596355173@qq.com>
2023-01-13 23:22:08 +0800

    feat: toggle component

---
+++ src/components/universal/Toggle/index.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
+import clsx from 'clsx'
+import styles from "./index.module.css"
+
+interface SwitchProps {
+  checked: boolean
+  onChange: (checked: boolean) => void
+}
+
+export const Toggle: React.FC<SwitchProps> = ({ checked, onChange }) => {
+  const [state, setState] = useState(checked)
+  const [enter, setEnter] = useState(false)
+
+  const handleChange = () => {
+    setState(!state)
+    onChange(!state)
+  }
+
+  return (
+    <Switch
+      checked={state}
+      onChange={handleChange}
+      className={clsx(styles.switch, { [styles.active]: state })}
+      onMouseDown={() => setEnter(true)}
+      onMouseUp={() => setEnter(false)}
+      onTouchStart={() => setEnter(true)}
+      onTouchEnd={() => setEnter(false)}
+      
+    >
+      <span className="sr-only">Use setting</span>
+      <span
+        className={clsx(styles.circle, { [styles.cirCleActive]: state }, { [styles.enter]: enter })}
+      />
+    </Switch>
+  )
+}
