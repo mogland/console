f62100339cef55a2143b930ae500b3d75b280aac
wibus-wee <1596355173@qq.com>
2023-01-14 16:14:09 +0800

    feat(Tags): tags support auto complete

--- src/components/universal/Tags/index.tsx
+++ src/components/universal/Tags/index.tsx
@@ -1,7 +1,10 @@
 import { Close, Plus } from "@icon-park/react"
-import type { PropsWithChildren, PropsWithRef } from "react";
-import { forwardRef, useRef, useState } from "react";
+import type { PropsWithChildren, PropsWithRef} from "react";
+import { useEffect , forwardRef, useRef, useState } from "react";
 import { useKeyPressEvent } from "react-use";
+import { useSnapshot } from "valtio";
+import { server } from "../../../states/app";
+import { apiClient } from "../../../utils/request";
 import styles from "./index.module.css"
 
 interface TagsProp {
@@ -29,12 +32,21 @@ export const Tags: React.FC<TagsProp> = (props) => {
   }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
     setInputValue(e.target.value)
   }
 
   const handleInputConfirm = () => {
     if (inputValue && tags.indexOf(inputValue) === -1) {
       setTags([...tags, inputValue])
+    }
+    setInputVisible(false)
+    setInputValue("")
+  }
+
+  const handleConfirmWithAutoComplete = (value?: string) => {
+    if (inputValue && tags.indexOf(inputValue) === -1 || value && tags.indexOf(value) === -1) {
+      setTags([...tags, value || inputValue])
     }
     setInputVisible(false)
     setInputValue("")
@@ -53,8 +65,9 @@ export const Tags: React.FC<TagsProp> = (props) => {
               style={{ width: 78 }}
               defaultValue={inputValue}
               onChange={handleInputChange}
-              onBlur={handleInputConfirm}
+              // onBlur={handleInputConfirm}
               onPressEnter={handleInputConfirm}
+              onAutoComplete={handleConfirmWithAutoComplete}
             />
           )
         }
@@ -78,16 +91,31 @@ export const Tags: React.FC<TagsProp> = (props) => {
   )
 }
 
-export const Input = forwardRef<HTMLInputElement, PropsWithRef<PropsWithChildren & React.HTMLAttributes<HTMLInputElement>> & { onPressEnter?: () => void }>((props, ref) => {
-  const { ...rest } = props
-  const { onPressEnter } = props
+export const Input = forwardRef<HTMLInputElement, PropsWithRef<PropsWithChildren & React.HTMLAttributes<HTMLInputElement>> & { onPressEnter?: () => void, onAutoComplete?: (value?: string) => void }>((props, ref) => {
+  const tags = useSnapshot(server).tags
+  const { onPressEnter, ...rest } = props
   useKeyPressEvent("Enter", onPressEnter)
   return (
-    <input
-      className={styles.input}
-      ref={ref}
-      {...rest}
-    />
+    <div className={styles.suggestContainer}>
+      <input
+        className={styles.input}
+        ref={ref}
+        {...rest}
+      />
+      <div className={styles.suggest}>
+        {tags.map(tag => (
+          <div
+            key={tag.name}
+            className={styles.input}
+            onClick={() => {
+              props.onAutoComplete?.(tag.name)
+            }}
+          >
+            {tag.name}
+          </div>
+        )).sort((a, b) => b.props.children.length - a.props.children.length)}
+      </div>
+    </div>
   )
 })
 
