629efe1bba5d6745b1adf49ae96f112f6ed6ebf4
wibus-wee <1596355173@qq.com>
2023-01-15 21:04:51 +0800

    feat: collapse universal component

---
+++ src/components/universal/Collapse/index.tsx
@@ -0,0 +1,30 @@
+import { Disclosure } from "@headlessui/react"
+import { PropsWithChildren } from "react"
+import styles from "./index.module.css"
+
+interface ICollapse {
+  title: string
+  children: React.ReactNode
+  open?: boolean
+}
+
+export const Collapse: React.FC<ICollapse> = (props) => {
+  return (
+    <Disclosure>
+      <Disclosure.Button className={styles.button}>
+        {props.title}
+      </Disclosure.Button>
+      <Disclosure.Panel className={styles.panel}>
+        {props.children}
+      </Disclosure.Panel>
+    </Disclosure>
+  )
+}
+
+export const CollapseContainer: React.FC<PropsWithChildren> = (props) => {
+  return (
+    <div className={styles.container}>
+      {props.children}
+    </div>
+  )
+}
