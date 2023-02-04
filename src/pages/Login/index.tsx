4ab6bf0d27eee616fa2f2fcefab7d24df638c915
wibus-wee <1596355173@qq.com>
2023-02-04 23:15:40 +0800

    perf: ,

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,25 +1,24 @@
 import {
   Box,
   Button,
-  Divider,
   Container,
   FormControl,
   FormLabel,
   Heading,
-  HStack,
   Input,
   Stack,
   Text,
   useToast,
+  useColorMode,
 } from '@chakra-ui/react'
 import { useRef } from 'react'
-import { Logo } from '@components/widgets/Logo'
 import { PasswordField } from '@components/widgets/PasswordField'
-import { OAuthButtonGroup } from '@components/widgets/OAuthButtonGroup'
 import { apiClient } from '@utils/request'
 import { useNavigate } from 'react-router-dom'
+import { useSeo } from '@hooks/use-seo'
 
 export const Login = () => {
+  useSeo("登录")
   const toast = useToast();
   const navigate = useNavigate();
   const userNameRef = useRef<HTMLInputElement>(null)
@@ -56,7 +55,9 @@ export const Login = () => {
     <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
       <Stack spacing="8">
         <Stack spacing="6">
-          <Logo />
+          <Stack direction="row" align="center" justify="center">
+            <Text fontSize="6xl">🏝</Text>
+          </Stack>
           <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
             <Heading fontSize={{ base: '2xl' }}>Sign in to your account</Heading>
           </Stack>
@@ -64,7 +65,7 @@ export const Login = () => {
         <Box
           py={{ base: '0', sm: '8' }}
           px={{ base: '4', sm: '10' }}
-          bg={{ base: 'gray.800', sm: 'bg-surface' }}
+          bg={{ base: useColorMode().colorMode === 'light' ? 'white' : 'gray.800', sm: 'bg-surface' }}
           boxShadow={{ base: 'none', sm: 'md' }}
           borderRadius={{ base: 'none', sm: 'xl' }}
         >
@@ -77,18 +78,18 @@ export const Login = () => {
               <PasswordField ref={passwordRef} />
             </Stack>
             <Stack spacing="6">
-              <Button 
+              <Button
                 colorScheme={"blue"}
                 onClick={handleLogin}
               >Sign in</Button>
-              <HStack>
+              {/* <HStack>
                 <Divider />
                 <Text fontSize="sm" whiteSpace="nowrap" color="muted">
-                  or continue with
+                  OR
                 </Text>
                 <Divider />
               </HStack>
-              <OAuthButtonGroup />
+              <OAuthButtonGroup /> */}
             </Stack>
           </Stack>
         </Box>
