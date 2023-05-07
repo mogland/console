1dc84f7a959c29bff801f24f0e0d2c58295ec808
wibus-wee <1596355173@qq.com>
2023-05-07 19:02:51 +0800

    feat: upload

--- src/pages/Files/index.tsx
+++ src/pages/Files/index.tsx
@@ -8,17 +8,19 @@ import { FilesBreadcrumb, Item } from "./components";
 import { getQueryVariable } from "@utils/url";
 import { useEffect, useState } from "react";
 import clsx from "clsx";
-import { ActionButtons } from "@components/widgets/ActionButtons";
+import { ActionButton, ActionButtons } from "@components/widgets/ActionButtons";
 import { apiClient } from "@utils/request";
 import { toast } from "sonner";
+import { AddOne, Upload } from "@icon-park/react";
 
 export const FilesPage: BasicPage = () => {
   useSeo("文件 · 管理");
   const path = getQueryVariable("path") || "";
-  const { data } = useSWR(`/store/list${path}`);
+  const { data, mutate } = useSWR(`/store/list${path}`);
   const [select, setSelect] = useState<string[]>([]);
 
   useEffect(() => {
+    mutate();
     setSelect([]);
   }, [path]);
 
@@ -31,6 +33,35 @@ export const FilesPage: BasicPage = () => {
             <FilesBreadcrumb path={path} />
           </div>
           <div>
+            <ActionButton
+              label="上传文件"
+              icon={<Upload />}
+              action={() => {
+                const input = document.createElement("input");
+                input.type = "file";
+                input.onchange = async (e) => {
+                  const file = (e.target as HTMLInputElement).files?.[0];
+                  if (!file) return;
+                  const formData = new FormData();
+                  formData.append("file", file);
+                  formData.append("path", path);
+                  toast.promise(
+                    apiClient(`/store/upload?path=${path}`, {
+                      method: "POST",
+                      body: formData,
+                    }).then(() => {
+                      mutate();
+                    }),
+                    {
+                      loading: "正在上传",
+                      success: "上传成功",
+                      error: "上传失败",
+                    }
+                  );
+                };
+                input.click();
+              }}
+            />
             <ActionButtons
               selectedClassName={styles.selected}
               selected={select}
@@ -42,17 +73,17 @@ export const FilesPage: BasicPage = () => {
                       await apiClient(`/store/delete?path=${item}`, {
                         method: "POST",
                         body: JSON.stringify({
-                          path: `${path}/${item}`
+                          path: `${path}/${item}`,
                         }),
                       });
-                    }
-                    )
-                  ), {
-                  loading: "正在删除",
-                  success: "删除成功",
-                  error: "删除失败",
+                    })
+                  ),
+                  {
+                    loading: "正在删除",
+                    success: "删除成功",
+                    error: "删除失败",
                   }
-                )
+                );
               }}
             />
           </div>
