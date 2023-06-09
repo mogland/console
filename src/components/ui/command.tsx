d4702b231e197dab6a0054de709e55fa8dd86595
wibus-wee <1596355173@qq.com>
2023-06-09 21:58:52 +0800

    feat: keydown

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,41 +1,70 @@
-"use client"
+"use client";
 
-import type { DialogProps } from "@radix-ui/react-dialog"
-import { Command as CommandPrimitive } from "cmdk"
-import { Search } from "lucide-react"
+import type { DialogProps } from "@radix-ui/react-dialog";
+import { Command as CommandPrimitive } from "cmdk";
+import { Search } from "lucide-react";
 
-import { cn } from "@libs/cn"
-import { Dialog, DialogContent } from "@components/ui/dialog"
-import { forwardRef } from "react"
+import { cn } from "@libs/cn";
+import { Dialog, DialogContent } from "@components/ui/dialog";
+import { forwardRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { jump } from "@utils/path";
 
 const Command = forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive>
->(({ className, ...props }, ref) => (
-  <CommandPrimitive
-    ref={ref}
-    className={cn(
-      "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
-      className
-    )}
-    {...props}
-  />
-))
-Command.displayName = CommandPrimitive.displayName
+>(({ className, value, onValueChange, ...props }, ref) => {
+  return (
+    <CommandPrimitive
+      ref={ref}
+      className={cn(
+        "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
+        className
+      )}
+      value={value}
+      onValueChange={onValueChange}
+      {...props}
+    />
+  );
+});
+Command.displayName = CommandPrimitive.displayName;
 
-interface CommandDialogProps extends DialogProps {}
+interface CommandDialogProps extends DialogProps {
+  onCommandKeyDownCapture?: React.KeyboardEventHandler;
+}
 
-const CommandDialog = ({ children, ...props }: CommandDialogProps & React.PropsWithChildren<typeof Command>) => {
+const CommandDialog = ({
+  children,
+  onCommandKeyDownCapture,
+  ...props
+}: CommandDialogProps & React.PropsWithChildren<typeof Command>) => {
+  const navigate = useNavigate();
+  const [value, setValue] = useState("");
+  const onValueChange = (value: string) => {
+    setValue(value);
+  };
+
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-2xl">
-        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+        <Command
+          value={value}
+          onValueChange={onValueChange}
+          onKeyDownCapture={(e) => {
+            if (e.keyCode === 13) {
+              e.preventDefault();
+              navigate(jump(value));
+              onCommandKeyDownCapture?.(e);
+            }
+          }}
+          className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5"
+        >
           {children}
         </Command>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
 const CommandInput = forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
@@ -53,9 +82,9 @@ const CommandInput = forwardRef<
       {...props}
     />
   </div>
-))
+));
 
-CommandInput.displayName = CommandPrimitive.Input.displayName
+CommandInput.displayName = CommandPrimitive.Input.displayName;
 
 const CommandList = forwardRef<
   React.ElementRef<typeof CommandPrimitive.List>,
@@ -66,9 +95,9 @@ const CommandList = forwardRef<
     className={cn("max-h-[300px] overflow-y-auto overflow-x-hidden", className)}
     {...props}
   />
-))
+));
 
-CommandList.displayName = CommandPrimitive.List.displayName
+CommandList.displayName = CommandPrimitive.List.displayName;
 
 const CommandEmpty = forwardRef<
   React.ElementRef<typeof CommandPrimitive.Empty>,
@@ -79,9 +108,9 @@ const CommandEmpty = forwardRef<
     className="py-6 text-center text-sm"
     {...props}
   />
-))
+));
 
-CommandEmpty.displayName = CommandPrimitive.Empty.displayName
+CommandEmpty.displayName = CommandPrimitive.Empty.displayName;
 
 const CommandGroup = forwardRef<
   React.ElementRef<typeof CommandPrimitive.Group>,
@@ -95,9 +124,9 @@ const CommandGroup = forwardRef<
     )}
     {...props}
   />
-))
+));
 
-CommandGroup.displayName = CommandPrimitive.Group.displayName
+CommandGroup.displayName = CommandPrimitive.Group.displayName;
 
 const CommandSeparator = forwardRef<
   React.ElementRef<typeof CommandPrimitive.Separator>,
@@ -108,24 +137,26 @@ const CommandSeparator = forwardRef<
     className={cn("-mx-1 h-px bg-border", className)}
     {...props}
   />
-))
-CommandSeparator.displayName = CommandPrimitive.Separator.displayName
+));
+CommandSeparator.displayName = CommandPrimitive.Separator.displayName;
 
 const CommandItem = forwardRef<
   React.ElementRef<typeof CommandPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
->(({ className, ...props }, ref) => (
-  <CommandPrimitive.Item
-    ref={ref}
-    className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-      className
-    )}
-    {...props}
-  />
-))
+>(({ className, ...props }, ref) => {
+  return (
+    <CommandPrimitive.Item
+      ref={ref}
+      className={cn(
+        "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  );
+});
 
-CommandItem.displayName = CommandPrimitive.Item.displayName
+CommandItem.displayName = CommandPrimitive.Item.displayName;
 
 const CommandShortcut = ({
   className,
@@ -139,9 +170,9 @@ const CommandShortcut = ({
       )}
       {...props}
     />
-  )
-}
-CommandShortcut.displayName = "CommandShortcut"
+  );
+};
+CommandShortcut.displayName = "CommandShortcut";
 
 export {
   Command,
@@ -153,4 +184,4 @@ export {
   CommandItem,
   CommandShortcut,
   CommandSeparator,
-}
+};
--- src/components/widgets/CommandDialog/index.tsx
+++ src/components/widgets/CommandDialog/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   CommandDialog as CommandDialogPrimitive,
@@ -8,68 +8,82 @@ import {
   CommandItem,
   CommandList,
   CommandSeparator,
-} from "@components/ui/command"
-import { useEffect, useState } from "react"
-import { SIDEBAR } from "../../../sidebar"
-import { useSnapshot } from "valtio"
-import { server } from "@states/app"
+} from "@components/ui/command";
+import { useEffect, useState } from "react";
+import { SIDEBAR } from "../../../sidebar";
+import { useSnapshot } from "valtio";
+import { server } from "@states/app";
 
 export function CommandDialog() {
-  const [open, setOpen] = useState(false)
-  const serverSnapshot = useSnapshot(server)
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "k" && e.metaKey) {
-        setOpen((open) => !open)
+        setOpen((open) => !open);
       }
-    }
+    };
 
-    document.addEventListener("keydown", down)
-    return () => document.removeEventListener("keydown", down)
-  }, [])
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
 
   return (
     <>
-      <CommandDialogPrimitive open={open} onOpenChange={setOpen}>
+      <CommandDialogPrimitive
+        open={open}
+        onOpenChange={setOpen}
+        onCommandKeyDownCapture={() => {
+          setOpen(false);
+        }}
+      >
         <CommandInput placeholder="Type a command or search..." />
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup heading="Pages">
-            {
-              SIDEBAR.map((item) => (
-                <>
-                  {item.map((item) => (
-                    <>
-                      <CommandItem key={item.title}>
-                        <span>{item.title} <code className="text-gray-400">{item.path}</code></span>
+            {SIDEBAR.map((item) => (
+              <>
+                {item.map((item) => (
+                  <>
+                    <CommandItem value={item.path} key={item.title}>
+                      <span>
+                        {item.title}{" "}
+                        <code className="text-gray-400">{item.path}</code>
+                      </span>
+                    </CommandItem>
+                    {item.subItems?.map((item) => (
+                      <CommandItem value={item.path} key={item.title}>
+                        <span>
+                          {item.title}{" "}
+                          <code className="text-gray-400">{item.path}</code>
+                        </span>
                       </CommandItem>
-                      {
-                        item.subItems?.map((item) => (
-                          <CommandItem key={item.title}>
-                            <span>{item.title} <code className="text-gray-400">{item.path}</code></span>
-                          </CommandItem>
-                        ))
-                      }
-                    </>
-                  ))}
-                </>
-              ))
-            }
+                    ))}
+                  </>
+                ))}
+              </>
+            ))}
           </CommandGroup>
-          <CommandSeparator />
           <CommandGroup heading="Category">
-            {
-              server.categories.map((category) => (
-                <>
-                  <CommandItem key={category.name}>
-                    <span>{category.name} <code className="text-gray-400">{category.slug}</code></span>
-                  </CommandItem>
-                </>
-              ))
-            }
+            {server.categories.map((category, index) => (
+              <>
+                <CommandItem 
+                value={"/categories"}
+                key={index}>
+                  <span
+                    onClick={() => {
+                      console.log(category);
+                    }}
+                  >
+                    {category.name}{" "}
+                    <code className="text-gray-400">{category.slug}</code>
+                  </span>
+                </CommandItem>
+              </>
+            ))}
           </CommandGroup>
         </CommandList>
       </CommandDialogPrimitive>
     </>
-  )
+  );
 }
