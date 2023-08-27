1baedf8fea1efd6f714f506f6e8e8296f1c1e51c
wibus-wee <1596355173@qq.com>
2023-08-27 17:04:49 +0800

    refactor(textarea): refactor original textarea component to radix-ui

--- src/components/widgets/ThemeComponent/index.tsx
+++ src/components/widgets/ThemeComponent/index.tsx
@@ -1,9 +1,5 @@
-import type { CheckBoxProps } from "@components/universal/CheckBox";
-import { CheckBox } from "@components/universal/CheckBox";
 import type { SwitchProps } from "@components/universal/Toggle";
 import { Toggle } from "@components/universal/Toggle";
-import type { InputInterface, TextareaProps } from "@pages/Write/Input";
-import { Textarea, Input } from "@pages/Write/Input";
 import type { RadioProps } from "@components/universal/Radio";
 import { Radio } from "@components/universal/Radio";
 import type { ColorProps } from "@components/universal/Color";
@@ -11,15 +7,18 @@ import { Color } from "@components/universal/Color";
 import { ModalBody } from "@components/universal/Modal";
 import type { SelectProps } from "./ThemeSelect";
 import { Select } from "./ThemeSelect";
+import { Input, InputProps } from "@components/ui/input";
+import { Textarea, TextareaProps } from "@components/ui/textarea";
+import { Checkbox } from "@components/ui/checkbox";
+import { CheckboxProps } from "@radix-ui/react-checkbox";
 
 const components = {
-  input: (props: InputInterface) => <Input {...props} className="font-mono" />,
+  input: (props: InputProps) => <Input {...props} className="font-mono" />,
   select: (props: SelectProps) => <Select {...props} />,
-  textarea: (props: TextareaProps) => (
-    <Textarea {...props} className="font-mono h-24" />
-  ),
+  textarea: (props: TextareaProps) => <Textarea {...props} className="font-mono h-24" />,
+  checkbox: (props: CheckboxProps) => <Checkbox {...props} />,
+  
   switch: (props: SwitchProps) => <Toggle {...props} />,
-  checkbox: (props: CheckBoxProps) => <CheckBox {...props} />,
   radio: (props: RadioProps) => <Radio {...props} />,
   color: (props: ColorProps) => <Color {...props} />,
 };
